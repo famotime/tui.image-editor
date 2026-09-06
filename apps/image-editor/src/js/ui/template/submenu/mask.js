9a6bab4d730b620d387ae63adf44e4da9462f548
Quincy Zou <quincy.zou@gmail.com>
2026-09-06 11:40:18 +0800

    fix: correct vertical alignment for submenu checkboxes and apply buttons

--- apps/image-editor/src/js/ui/template/submenu/mask.js
+++ apps/image-editor/src/js/ui/template/submenu/mask.js
@@ -18,7 +18,7 @@ export default ({ locale, makeSvgIcon }) => `
         <li class="tui-image-editor-partition only-left-right">
             <div></div>
         </li>
-        <li class="tie-mask-apply tui-image-editor-newline apply" style="margin-top: 22px;margin-bottom: 5px">
+        <li class="tie-mask-apply tui-image-editor-newline apply">
             <div class="tui-image-editor-button apply">
                 ${makeSvgIcon(['normal', 'active'], 'apply')}
                 <label>
